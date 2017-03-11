ad6539b020afde1ad35fe47540027f3dbbce41ec
Michael Rojas <mikerojaswa@gmail.com>
2017-03-10 21:12:41 -0700

    Add register, home screen

--- BitBite/RegisterViewController.swift
+++ BitBite/RegisterViewController.swift
@@ -1,32 +1,24 @@
 //
-//  ViewController.swift
+//  RegisterViewController.swift
 //  BitBite
 //
-//  Created by Bobby Chen on 3/10/17.
+//  Created by Michael Rojas on 3/10/17.
 //  Copyright © 2017 BMStudios. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var passwordTextField: UITextField!
-    @IBOutlet weak var emailTextField: UITextField!
+class RegisterViewController: UIViewController {
     
-    @IBAction func unwindToLogin(segue: UIStoryboardSegue) {
-        //self.dismiss(animated: true, completion: nil)
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
