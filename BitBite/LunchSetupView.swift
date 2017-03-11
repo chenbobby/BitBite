0d48e3d6b6497c08c158d6afd062609ad0d0779a
Bobby Chen <chenbobby168@gmail.com>
2017-03-11 01:46:35 -0700

    create account setup views
    animations still need work

---
+++ BitBite/LunchSetupView.swift
@@ -0,0 +1,15 @@
+//
+//  LunchSetupView.swift
+//  BitBite
+//
+//  Created by Bobby Chen on 3/11/17.
+//  Copyright © 2017 BMStudios. All rights reserved.
+//
+
+import UIKit
+
+class LunchSetupView: UIView {
+    
+    
+    
+}
