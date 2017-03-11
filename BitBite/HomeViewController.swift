9fe504c3ead9c12f28198de0bab9f875c5e8530e
Michael Rojas <mikerojaswa@gmail.com>
2017-03-10 22:18:31 -0700

    Added home view controller

---
+++ BitBite/HomeViewController.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewController.swift
+//  BitBite
+//
+//  Created by Michael Rojas on 3/10/17.
+//  Copyright © 2017 BMStudios. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
