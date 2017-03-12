a083cc85f1b38464e3f23b0ebd00d2660ae3b16f
Bobby Chen <chenbobby168@gmail.com>
2017-03-11 22:42:45 -0700

    food picker algorithm working
    going to connect food picker and maps search

---
+++ BitBite/MenuViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MenuViewController.swift
+//  BitBite
+//
+//  Created by Bobby Chen on 3/11/17.
+//  Copyright © 2017 BMStudios. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+}
